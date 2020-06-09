15bedbc65b448116097c705422adf32dbd4f40d7
Touchpeach <lpt19831306448@gmail.com>
2020-06-09 18:53:03 +0800

    getWay部分代码写完，需要测试

---
+++ cloud-getway-getway9527/src/main/java/com/pengtao/springcloud/GetWayMain9527.java
@@ -0,0 +1,19 @@
+package com.pengtao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author pengtao.li
+ * @Description: mySpringCloud
+ * @Email lpt18731306448@gmail.com
+ * @Date 2020/6/6 22:34
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GetWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run( GetWayMain9527.class,args);
+    }
+}
